be491c7b60af925f8947f1bedebc123552d210d8
Pyotr Ermishkin <quasiyoke@gmail.com>
2014-10-14 11:28:02 +0400

    Post requests to Helloworld view were csrf-exempted.

--- ask_pupkin/views.py
+++ ask_pupkin/views.py
@@ -1,5 +1,14 @@
+from django.views.decorators import csrf as csrf_decorators
 from django.views.generic.base import TemplateView
 
 
 class Helloworld(TemplateView):
     template_name = 'helloworld.html'
+
+    @csrf_decorators.csrf_exempt
+    def dispatch(self, *args, **kwargs):
+        return super(Helloworld, self).dispatch(*args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)        
