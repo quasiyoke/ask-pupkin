f282b707ab21e6fa894097c5d6c48bf51cc21414
Pyotr Ermishkin <quasiyoke@gmail.com>
2014-10-14 11:40:02 +0400

    Apache WSGI config for Django was finally configured.

--- ask_pupkin/wsgi.py
+++ ask_pupkin/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ask_pupkin.settings")
 
 from django.core.wsgi import get_wsgi_application
