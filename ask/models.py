e7e6809526fdf71f3bc25537a612d54faa56bdf8
Pyotr Ermishkin <quasiyoke@gmail.com>
2014-12-23 14:12:25 +0300

    'upload_to' property was set up correctly for models.

--- ask/models.py
+++ ask/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class User(models.Model):
+    user = models.OneToOneField(User)
+    avatar = models.ImageField(upload_to='users_avatars')
+    rating = models.IntegerField()
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey('User')
+    created = models.DateTimeField()
+    tags = models.ManyToManyField('Tag')
+
+
+class Response(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey('User')
+    is_right = models.BooleanField(default=False)
+
+
+class Tag(models.Model):
+    text = models.CharField(max_length=100)
--- ask_pupkin/settings.py
+++ ask_pupkin/settings.py
@@ -80,6 +80,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
