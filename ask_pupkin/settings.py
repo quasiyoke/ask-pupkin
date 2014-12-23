30703b3a3d524dd1ebcaaf354b78440b491ea87f
Pyotr Ermishkin <quasiyoke@gmail.com>
2014-12-23 13:28:16 +0300

    Mysql was connected

--- ask_pupkin/settings.py
+++ ask_pupkin/settings.py
@@ -58,8 +58,10 @@ WSGI_APPLICATION = 'ask_pupkin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'askpupkin',
+        'USER': 'askpupkin',
+        'PASSWORD': 'topsecret'
     }
 }
 
