31ab14b6fd0925f9c3eb44e6cd22d901635a814e
Pyotr Ermishkin <quasiyoke@gmail.com>
2014-12-27 01:50:23 +0300

    First page questions' showing was implemented.

--- ask/views.py
+++ ask/views.py
@@ -1,7 +1,9 @@
 from django.views.decorators import csrf as csrf_decorators
 from django.views.generic import base as base_views
 from django.views.generic import edit as edit_views
+from django.views.generic import list as list_views
 import forms
+import models
 
 
 class Helloworld(base_views.TemplateView):
@@ -16,8 +18,10 @@ class Helloworld(base_views.TemplateView):
         return self.render_to_response(context)        
 
 
-class Home(base_views.TemplateView):
+class Home(list_views.ListView):
     template_name = 'index.html'
+    model = models.Question
+    paginate_by = 10
 
 
 class Login(edit_views.FormView):
